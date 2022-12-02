aba6709bba07ced25cf28c27277b52a230541bec
bharadwajchukkala <bchukkal@terpmail.umd.edu>
2022-12-02 17:21:46 -0500

    Task 1.5.3 Add Test files for the class templates

---
+++ garbage_collector/test/manipulation_test.cpp
@@ -0,0 +1,35 @@
+// Copyright 2015 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include <rclcpp/rclcpp.hpp>
+
+#include "std_msgs/msg/string.hpp"
+
+#include "../include/garbage_collector/Manipulation.hpp"
+
+class TaskManipulation : public testing::Test {
+ protected:
+  rclcpp::Node::SharedPtr node_;
+};
+
+TEST_F(TaskTalker, test_num_publishers) {
+  node_ = rclcpp::Node::make_shared("test_manipulation");
+  auto test_pub = node_->create_publisher<std_msgs::msg::String>
+                    ("manipulation", 10.0);
+
+  auto num_pub = node_->count_publishers("manipulation");
+  EXPECT_EQ(0, static_cast<int>(num_pub));
+}
