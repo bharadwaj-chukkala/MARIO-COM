ea97526956cbd83a9a8c49fe7ad9c5d533ed5730
bharadwajchukkala <bchukkal@terpmail.umd.edu>
2022-12-02 17:15:19 -0500

    Task 1.5.2.5: Add main function

---
+++ garbage_collector/src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Bharadwaj Chukkala (bchukkal@umd.edu) [Driver]
+ * @author Adarsh Malapaka (amalapak@umd.edu) [Navigator]
+ * @author Kumara Ritvik Oruganti (okritvik@umd.edu) [Design Keeper]
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "../include/garbage_collector/RobotSim.hpp"
+
+int main() {
+    // Code stub
+    RobotSim rs;
+}
