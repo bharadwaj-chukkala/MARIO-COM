cb311f156541aae0f2228fc4bacdd6936bac9155
bharadwajchukkala <bchukkal@terpmail.umd.edu>
2022-12-02 17:09:57 -0500

    Task 1.5.2.3: Add RobotSim class implementation

---
+++ garbage_collector/src/RobotSim.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file RobotSim.cpp
+ * @author Bharadwaj Chukkala (bchukkal@umd.edu) [Driver]
+ * @author Adarsh Malapaka (amalapak@umd.edu) [Navigator]
+ * @author Kumara Ritvik Oruganti (okritvik@umd.edu) [Design Keeper]
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "../include/garbage_collector/RobotSim.hpp"
+
+RobotSim::RobotSim() {
+    m_nav = Navigation();
+    m_perc = Perception();
+    m_manip = Manipulation();
+}
