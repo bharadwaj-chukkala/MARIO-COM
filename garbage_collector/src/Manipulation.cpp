17c87bc3d75f2f4d99b7d56a57010e2aeb1540d8
bharadwajchukkala <bchukkal@terpmail.umd.edu>
2022-12-02 17:08:26 -0500

    Task 1.5.2.1: Add Manipulation class template

---
+++ garbage_collector/src/Manipulation.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file Manipulation.cpp
+ * @author Bharadwaj Chukkala (bchukkal@umd.edu) [Driver]
+ * @author Adarsh Malapaka (amalapak@umd.edu) [Navigator]
+ * @author Kumara Ritvik Oruganti (okritvik@umd.edu) [Design Keeper]
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "../include/garbage_collector/Manipulation.hpp"
+
+Manipulation::Manipulation() {
+    m_pick_pose = nullptr;
+    m_place_pose = nullptr;
+}
+
+void Manipulation::gripper_open() {
+    // Code Stub
+}
+
+void Manipulation::gripper_close() {
+    // Code Stub
+}
+bool Manipulation::pick_bin() {
+    // Code Stub
+    return true;
+}
+bool Manipulation::place_bin() {
+    // Code Stub
+    return true;
+}
