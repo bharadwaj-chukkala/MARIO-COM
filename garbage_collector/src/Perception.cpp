810e93884c5021ebfc70be4995a824467d743b8e
bharadwajchukkala <bchukkal@terpmail.umd.edu>
2022-12-02 17:09:23 -0500

    Task 1.5.2.3: Add Perception class implementation

---
+++ garbage_collector/src/Perception.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file Perception.cpp
+ * @author Bharadwaj Chukkala (bchukkal@umd.edu) [Driver]
+ * @author Adarsh Malapaka (amalapak@umd.edu) [Navigator]
+ * @author Kumara Ritvik Oruganti (okritvik@umd.edu) [Design Keeper]
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "../include/garbage_collector/Perception.hpp"
+
+Perception::Perception() {
+    m_img_feed = nullptr;
+    m_lidar_feed = nullptr;
+}
+
+bool detect_bin() {
+    // Code Stub
+    return true;
+}
+
+void select_bin(geometry_msgs::msg::Pose pose) {
+    // Code Stub
+}
+
+bool move_to_bin() {
+    // Code Stub
+    return true;
+}
