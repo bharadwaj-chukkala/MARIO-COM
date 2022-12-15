3e6eb516e0c80e7a271329cf2ecc755248cbebe0
okritvik <okritvik@gmail.com>
2022-12-15 05:33:44 -0500

    Task 2.6.3: Unit tests for manipulation class

--- mario_com/test/manipulation_test.cpp
+++ mario_com/test/manipulation_test.cpp
@@ -30,15 +30,46 @@
 #include "../include/mario_com/Manipulation.hpp"
 
 class TaskManipulation : public testing::Test {
- protected:
+ public:
+  geometry_msgs::msg::Pose m_place_pose;
+  CLIENT_DELETE pick_client;
+  CLIENT_SPAWN place_client;
+  rclcpp::Node::SharedPtr manip_node;
   rclcpp::Node::SharedPtr node_;
 };
 
-TEST_F(TaskManipulation, test_num_publishers) {
+TEST_F(TaskManipulation, test_pick_bin) {
   node_ = rclcpp::Node::make_shared("test_manipulation");
+  m_place_pose.position.x = 3.5;
+  m_place_pose.position.y = -2.5;
+  m_place_pose.position.z = 0;
+  m_place_pose.orientation.x = 0;
+  m_place_pose.orientation.y = 0;
+  m_place_pose.orientation.z = 0;
+  m_place_pose.orientation.w = 0;
+  // Create the clients for picking and placing
+  pick_client = node_->create_client<SERVICE_DELETE>("delete_entity");
+  place_client = node_->create_client<SERVICE_SPAWN>("spawn_entity");
   auto test_pub = node_->create_publisher<std_msgs::msg::String>
                     ("manipulation", 10.0);
 
   auto num_pub = node_->count_publishers("manipulation");
   EXPECT_EQ(1, static_cast<int>(num_pub));
+}
+
+TEST_F(TaskManipulation, test_place_bin) {
+  node_ = rclcpp::Node::make_shared("test_manipulation");
+  m_place_pose.position.x = 3.5;
+  m_place_pose.position.y = -2.5;
+  m_place_pose.position.z = 0;
+  m_place_pose.orientation.x = 0;
+  m_place_pose.orientation.y = 0;
+  m_place_pose.orientation.z = 0;
+  m_place_pose.orientation.w = 0;
+  // Create the clients for picking and placing
+  pick_client = node_->create_client<SERVICE_DELETE>("delete_entity");
+  place_client = node_->create_client<SERVICE_SPAWN>("spawn_entity");
+  auto test_pub = node_->create_publisher<std_msgs::msg::String>
+                    ("manipulation", 10.0);
+  EXPECT_EQ(3.5, m_place_pose.position.x);
 }
