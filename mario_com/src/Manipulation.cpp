f296388757d9760116ccf4af2c16c1bd2c8a2a10
okritvik <okritvik@gmail.com>
2022-12-14 22:09:48 -0500

    Task 2.6.2: Include comments in the implementation

--- mario_com/src/Manipulation.cpp
+++ mario_com/src/Manipulation.cpp
@@ -18,6 +18,7 @@
 #include <rclcpp/future_return_code.hpp>
 
 Manipulation::Manipulation() : Node("manipulation") {
+    // Set the Trash Bin place position
     m_place_pose.position.x = 3.5;
     m_place_pose.position.y = -2.5;
     m_place_pose.position.z = 0;
@@ -25,6 +26,7 @@ Manipulation::Manipulation() : Node("manipulation") {
     m_place_pose.orientation.y = 0;
     m_place_pose.orientation.z = 0;
     m_place_pose.orientation.w = 0;
+    // Create the clients for picking and placing
     pick_client = create_client<SERVICE_DELETE>("delete_entity");
     place_client = create_client<SERVICE_SPAWN>("spawn_entity");
     manip_node = rclcpp::Node::
@@ -32,6 +34,7 @@ Manipulation::Manipulation() : Node("manipulation") {
 }
 
 bool Manipulation::pick_bin() {
+    // Check if the service is available and wait
     while (!pick_client->wait_for_service(1s)) {
         if (!rclcpp::ok()) {
           RCLCPP_ERROR(this->get_logger(),
@@ -42,10 +45,12 @@ bool Manipulation::pick_bin() {
         RCLCPP_INFO(this->get_logger(),
               "Server not available, waiting again...");
     }
+    // Create a request to delete the entity
     auto request = std::make_shared<REQUEST_DELETE>();
     request->name = "trash_bin1";
 
     auto result = pick_client->async_send_request(request);
+    // Wait till the entity is deleted and the status is success
     auto ret = rclcpp::spin_until_future_complete(manip_node,
                                             result, 10s);
     if (ret == rclcpp::FutureReturnCode::SUCCESS) {
@@ -56,6 +61,7 @@ bool Manipulation::pick_bin() {
 }
 
 bool Manipulation::place_bin() {
+    // Place the bin at the disposal area using system command
     auto res = system("ros2 run gazebo_ros spawn_entity.py -entity trash_bin -x 3.5 -y -2.5 -z 0 -file `ros2 pkg prefix mario_com`/share/mario_com/models/bin_cylinder/model.sdf");
     if (res > 0) {
         return true;
