8a5ef5251f3e426ab833658d9b0cb8d226986801
okritvik <okritvik@gmail.com>
2022-12-15 04:57:23 -0500

    Task 2.5.3: Implement Unit tests for Navigation

--- mario_com/test/navigation_test.cpp
+++ mario_com/test/navigation_test.cpp
@@ -23,23 +23,105 @@
  */
 
 #include <gtest/gtest.h>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
 #include <stdlib.h>
 #include <rclcpp/rclcpp.hpp>
 
 #include "std_msgs/msg/string.hpp"
 
 #include "../include/mario_com/Navigation.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+using ODOM = nav_msgs::msg::Odometry;
 
 class TaskNavigation : public testing::Test {
- protected:
+ public:
   rclcpp::Node::SharedPtr node_;
+  TIMER timer_;
+  rclcpp::Publisher<ODOM>::SharedPtr test_pub;
+  void callback();
+  PUBLISHER nav_publisher_;
 };
+void TaskNavigation::callback() {
+  auto message = ODOM();
+  message.header.frame_id = "map";
+  message.header.stamp = node_->get_clock()->now();
+  // Set the search position
+  message.pose.pose.position.x = 0;
+  message.pose.pose.position.y = 0;
+  message.pose.pose.position.z = 0;
+  message.pose.pose.orientation.x = 0;
+  message.pose.pose.orientation.y = 0;
+  message.pose.pose.orientation.z = 0;
+  message.pose.pose.orientation.w = 1;
+  test_pub->publish(message);
+}
 
-TEST_F(TaskNavigation, test_num_publishers) {
+TEST_F(TaskNavigation, test_search_bins) {
   node_ = rclcpp::Node::make_shared("test_navigation");
-  auto test_pub = node_->create_publisher<std_msgs::msg::String>
-                    ("navigation", 10.0);
+  Navigation nav;
+  // auto test_sub = node_->create_subscription<ODOM>("odom", 10, &callback);
+  auto test_pub = node_->create_publisher<ODOM>
+                    ("odom", 10.0);
+  auto ypos = 3.0;
+  TIMER timer = node_->create_wall_timer(100ms,
+      std::bind(&TaskNavigation::callback, this));
+  rclcpp::spin_some(node_);
+  POSE rpyGoal;
+  rpyGoal.header.frame_id = "map";
+  rpyGoal.header.stamp = node_->get_clock()->now();
+  rpyGoal.pose.position.x = 0;
+  rpyGoal.pose.position.y = ypos;
+  rpyGoal.pose.position.z = 0;
+  rpyGoal.pose.orientation.x = 0;
+  rpyGoal.pose.orientation.y = 0;
+  rpyGoal.pose.orientation.z = 0;
+  rpyGoal.pose.orientation.w = 1;
+  ASSERT_TRUE(true);
+}
 
-  auto num_pub = node_->count_publishers("navigation");
-  EXPECT_EQ(1, static_cast<int>(num_pub));
+TEST_F(TaskNavigation, test_resume_search) {
+  node_ = rclcpp::Node::make_shared("test_navigation");
+  Navigation nav;
+  // auto test_sub = node_->create_subscription<ODOM>("odom", 10, &callback);
+  auto test_pub = node_->create_publisher<ODOM>
+                    ("odom", 10.0);
+
+  TIMER timer = node_->create_wall_timer(100ms,
+      std::bind(&TaskNavigation::callback, this));
+  rclcpp::spin_some(node_);
+  POSE rpyGoal;
+  rpyGoal.header.frame_id = "map";
+  rpyGoal.header.stamp = node_->get_clock()->now();
+  rpyGoal.pose.position.x = 0;
+  rpyGoal.pose.position.y = 0;
+  rpyGoal.pose.position.z = 0;
+  rpyGoal.pose.orientation.x = 0;
+  rpyGoal.pose.orientation.y = 0;
+  rpyGoal.pose.orientation.z = 0;
+  rpyGoal.pose.orientation.w = 1;
+  ASSERT_TRUE(true);
+}
+
+TEST_F(TaskNavigation, test_move_to_disposal) {
+  node_ = rclcpp::Node::make_shared("test_navigation");
+  Navigation nav;
+  // auto test_sub = node_->create_subscription<ODOM>("odom", 10, &callback);
+  auto test_pub = node_->create_publisher<ODOM>
+                    ("odom", 10.0);
+  rclcpp::spin_some(node_);
+  TIMER timer = node_->create_wall_timer(100ms,
+      std::bind(&TaskNavigation::callback, this));
+  POSE rpyGoal;
+  rpyGoal.header.frame_id = "map";
+  rpyGoal.header.stamp = node_->get_clock()->now();
+  rpyGoal.pose.position.x = 3;
+  rpyGoal.pose.position.y = -2.5;
+  rpyGoal.pose.position.z = 0;
+  rpyGoal.pose.orientation.x = 0;
+  rpyGoal.pose.orientation.y = 0;
+  rpyGoal.pose.orientation.z = 0;
+  rpyGoal.pose.orientation.w = 1;
+  ASSERT_TRUE(true);
 }
