9e2512705e549e60e44f85a8c4e91c222b500060
okritvik <okritvik@gmail.com>
2022-12-14 22:08:53 -0500

    Task 2.8.2: Include comments in the integrated file

--- mario_com/src/main.cpp
+++ mario_com/src/main.cpp
@@ -14,8 +14,11 @@
 #include "../include/mario_com/RobotSim.hpp"
 #include <rclcpp/utilities.hpp>
 int main(int argc, char * argv[]) {
+    // Initialize the ROS
     rclcpp::init(argc, argv);
+    // Create Robot Simulator Object
     RobotSim rs;
+    // Start the operation
     RCLCPP_INFO(rclcpp::get_logger("log"), "Starting Trash Collection");
     while (!rs.m_nav.search_bins()) {
         if (rs.m_perc.detect_bin()) {
@@ -27,7 +30,8 @@ int main(int argc, char * argv[]) {
     rs.m_nav.move_to_disposal_zone();
     rs.m_manip.place_bin();
     rs.m_nav.resume_search();
+    // Shutdown ROS
     rclcpp::shutdown();
-    cv::destroyWindow("view");
+    // cv::destroyWindow("view");
     return 0;
 }
