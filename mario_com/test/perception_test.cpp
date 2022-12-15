e4b47d3d4be7545578d0cf5ee6d54062f16fd6e3
okritvik <okritvik@gmail.com>
2022-12-15 05:15:39 -0500

    Task 2.7.3: Add unit tests for perception class

--- mario_com/test/perception_test.cpp
+++ mario_com/test/perception_test.cpp
@@ -24,21 +24,114 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 #include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 #include "std_msgs/msg/string.hpp"
 
 #include "../include/mario_com/Perception.hpp"
 
 class TaskPerception : public testing::Test {
- protected:
+ public:
   rclcpp::Node::SharedPtr node_;
+  cv::Mat m_img_feed;
+  sensor_msgs::msg::LaserScan m_lidar_feed;
+  rclcpp::NodeOptions options;
+  image_transport::Subscriber sub;
+  rclcpp::Node::SharedPtr img_node;
+  rclcpp::Publisher<TWIST>::SharedPtr m_pub_vel;
+  rclcpp::Node::SharedPtr percep_odom_node;
+  rclcpp::Subscription<ODOM>::SharedPtr odom_sub;
+  bool r_rotate_flag;
+  bool l_rotate_flag;
+  bool move_forward;
+  bool stop_flag;
+  bool next_location;
+  double present_yaw;
+  double initial_yaw;
+  void odom_callback_search(const ODOM::SharedPtr msg);
 };
 
-TEST_F(TaskPerception, test_num_publishers) {
+void TaskPerception::odom_callback_search(const ODOM::SharedPtr msg) {
+    tf2::Quaternion q(
+        msg->pose.pose.orientation.x,
+        msg->pose.pose.orientation.y,
+        msg->pose.pose.orientation.z,
+        msg->pose.pose.orientation.w);
+    tf2::Matrix3x3 m(q);
+    double r, p, y;
+    m.getRPY(r, p, y);
+    present_yaw = y;
+}
+
+
+TEST_F(TaskPerception, test_detect_bin) {
+  img_node = rclcpp::Node::make_shared("image_listener", options);
+  m_pub_vel = img_node->create_publisher<TWIST>("cmd_vel", 10);
+  percep_odom_node = rclcpp::Node::make_shared("percep_odom_node");
+  odom_sub = percep_odom_node->create_subscription<ODOM>("odom", 10,
+      std::bind(&TaskPerception::odom_callback_search, this, _1));
   node_ = rclcpp::Node::make_shared("test_publisher");
   auto test_pub = node_->create_publisher<std_msgs::msg::String>
                     ("perception", 10.0);
 
   auto num_pub = node_->count_publishers("perception");
   EXPECT_EQ(1, static_cast<int>(num_pub));
+
+  rclcpp::spin_some(percep_odom_node);
+    initial_yaw = present_yaw;
+    r_rotate_flag = false;
+    l_rotate_flag = false;
+    move_forward = false;
+    stop_flag = false;
+    next_location = false;
+
+    // cv::namedWindow("view");
+    // cv::startWindowThread();
+
+    image_transport::ImageTransport it(img_node);
+    // Start detecting the bin
+    EXPECT_FALSE(r_rotate_flag);
 }
+
+TEST_F(TaskPerception, test_move_bin) {
+  img_node = rclcpp::Node::make_shared("image_listener", options);
+  m_pub_vel = img_node->create_publisher<TWIST>("cmd_vel", 10);
+  percep_odom_node = rclcpp::Node::make_shared("percep_odom_node");
+  odom_sub = percep_odom_node->create_subscription<ODOM>("odom", 10,
+      std::bind(&TaskPerception::odom_callback_search, this, _1));
+  node_ = rclcpp::Node::make_shared("test_publisher");
+  auto test_pub = node_->create_publisher<std_msgs::msg::String>
+                    ("perception", 10.0);
+
+  auto num_pub = node_->count_publishers("perception");
+  EXPECT_EQ(1, static_cast<int>(num_pub));
+
+  rclcpp::spin_some(percep_odom_node);
+    initial_yaw = present_yaw;
+    r_rotate_flag = false;
+    l_rotate_flag = false;
+    move_forward = false;
+    stop_flag = false;
+    next_location = false;
+
+    auto vel = TWIST();
+    if (r_rotate_flag) {
+        vel.angular.z = -0.1;
+        vel.linear.x = 0;
+    } else if (l_rotate_flag) {
+        vel.angular.z = 0.1;
+        vel.linear.x = 0;
+    } else if (move_forward) {
+        vel.linear.x = 0.1;
+        vel.angular.z = 0;
+    } else if (stop_flag) {
+        vel.linear.x = 0;
+        vel.angular.z = 0;
+    }
+    m_pub_vel->publish(vel);
+
+    image_transport::ImageTransport it(img_node);
+    // Start detecting the bin
+    EXPECT_FALSE(stop_flag);
+}
+
